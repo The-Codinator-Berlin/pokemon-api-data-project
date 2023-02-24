094fd5a17693a092f88c45570c5134d1a1f10992
The-Codinator-Berlin <carl.manley.berlin@gmail.com>
2023-02-24 15:19:18 +0100

    comments added

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
 console.log(data);
 
-// Pokemon card images obtained with for loop 
-const cardsContainer = document.getElementById("cards-container")
-console.log(cardsContainer);
-for (let i = 0; i < data.data.length; i++) {
-    const img = document.createElement("img")
-    img.setAttribute("src", data.data[i].images.small);
-    cardsContainer.appendChild(img);
+    // Pokemon card images obtained with for loop 
+    //Created a variable for the cards container
+const cardsContainer = document.getElementById("cards-container") 
+    // logged cardsContainer to console
+console.log(cardsContainer); 
+    //This searches through all the length of the data
+for (let i = 0; i < data.data.length; i++) {   
+    // This creates an img element for every card
+    const img = document.createElement("img")   
+    // This specifies where the image is obtained from 
+    img.setAttribute("src", data.data[i].images.small); 
+    // This appends or puts the info into the page
+    cardsContainer.appendChild(img); 
 }
 
+console.log(typesOfPokemon);
