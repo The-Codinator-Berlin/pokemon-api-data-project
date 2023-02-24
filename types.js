a777e29026b6a6085da8b4afc603361b89e7de8f
The-Codinator-Berlin <carl.manley.berlin@gmail.com>
2023-02-24 14:31:39 +0100

    typesOfPokemon data JS

---
+++ types.js
@@ -0,0 +1,16 @@
+const typesOfPokemon =
+{
+  "data": [
+    "Colorless",
+    "Darkness",
+    "Dragon",
+    "Fairy",
+    "Fighting",
+    "Fire",
+    "Grass",
+    "Lightning",
+    "Metal",
+    "Psychic",
+    "Water"
+  ]
+}
