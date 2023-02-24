45355043d2c73e2c8f9ad89de9838dceb0355235
The-Codinator-Berlin <carl.manley.berlin@gmail.com>
2023-02-24 13:28:13 +0100

    Displayed pokemon cards in div looping with data

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
 console.log(data);
+
+// Pokemon card images obtained with for loop 
+const cardsContainer = document.getElementById("cards-container")
+console.log(cardsContainer);
+for (let i = 0; i < data.data.length; i++) {
+    const img = document.createElement("img")
+    img.setAttribute("src", data.data[i].images.small);
+    cardsContainer.appendChild(img);
+}
+
