53f016b5255624c98a8562c789ad2a82172f53bf
The-Codinator-Berlin <carl.manley.berlin@gmail.com>
2023-03-02 20:53:22 +0100

    Filter by specific type is functional

--- main.js
+++ main.js
@@ -1,23 +1,43 @@
 const allPokeCards = data.data;
 
-const typeToInclude = "Water"
-
-const filteredCards = allPokeCards.filter(function (card) {
-    if (card.types.includes(typeToInclude)) {
-        return true;
-    } 
-    return false;
-});
-
+const typeButtons = document.getElementsByName("myRadioField");
 const cardsContainer = document.getElementById("cards-container");
+const typeSelectionArea = document.getElementById("radio-container");
 
-// console.log(cardsContainer); 
+// listen for user changes to radio buttons (card type)
+typeSelectionArea.addEventListener("input", updateCardList);
 
-for (let i = 0; i < filteredCards.length; i++) {   
-    const img = document.createElement("img")   
-    img.setAttribute("src", filteredCards[i].images.small); 
-    cardsContainer.appendChild(img); 
+// update list when loading page
+updateCardList();
+
+function updateCardList() {
+    // get user-selected card type
+    let selectedType;
+    for (let i = 0; i < typeButtons.length; i++) {
+        if (typeButtons[i].checked) {
+            selectedType = typeButtons[i].value;
+        }
+    }
+
+    // filter the cards based selected type
+    const typeToInclude = selectedType || "Colorless";
+    const filteredCards = allPokeCards.filter(function (card) {
+        if (card.types.includes(typeToInclude)) {
+            return true;
+        }
+        return false;
+    });
+
+    // add images for filtered cards
+    const newImages = [];
+    for (let i = 0; i < filteredCards.length; i++) {
+        const img = document.createElement("img");
+        img.setAttribute("src", filteredCards[i].images.small);
+        newImages.push(img);
+    }
+    cardsContainer.replaceChildren(...newImages);
 }
+
 
 
 
