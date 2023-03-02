768c157395dbb8e8560aaa513951d26e2d5d1895
The-Codinator-Berlin <carl.manley.berlin@gmail.com>
2023-03-02 17:20:38 +0100

    Added filter function by card type

--- main.js
+++ main.js
@@ -1,18 +1,23 @@
-console.log(data);
+const allPokeCards = data.data;
 
-    // Pokemon card images obtained with for loop 
-    //Created a variable for the cards container
-const cardsContainer = document.getElementById("cards-container") 
-    // logged cardsContainer to console
-console.log(cardsContainer); 
-    //This searches through all the length of the data
-for (let i = 0; i < data.data.length; i++) {   
-    // This creates an img element for every card
+const typeToInclude = "Water"
+
+const filteredCards = allPokeCards.filter(function (card) {
+    if (card.types.includes(typeToInclude)) {
+        return true;
+    } 
+    return false;
+});
+
+const cardsContainer = document.getElementById("cards-container");
+
+// console.log(cardsContainer); 
+
+for (let i = 0; i < filteredCards.length; i++) {   
     const img = document.createElement("img")   
-    // This specifies where the image is obtained from 
-    img.setAttribute("src", data.data[i].images.small); 
-    // This appends or puts the info into the page
+    img.setAttribute("src", filteredCards[i].images.small); 
     cardsContainer.appendChild(img); 
 }
 
-console.log(typesOfPokemon);
+
+
