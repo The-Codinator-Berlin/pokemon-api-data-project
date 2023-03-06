408e1c9ca313206f349515aa087ead94845cca79
The-Codinator-Berlin <carl.manley.berlin@gmail.com>
2023-03-06 22:06:13 +0100

    Made clearer commets + refined code

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const allPokeCards = data.data;
-
+// console.log(allPokeCards.length);
 const typeButtons = document.getElementsByName("myRadioField");
 const cardsContainer = document.getElementById("cards-container");
 const typeSelectionArea = document.getElementById("radio-container");
@@ -11,30 +11,29 @@ typeSelectionArea.addEventListener("input", updateCardList);
 updateCardList();
 
 function updateCardList() {
-    // get user-selected card type
+    // Loop is assigning button checked to variable selectedType
     let selectedType;
     for (let i = 0; i < typeButtons.length; i++) {
-        if (typeButtons[i].checked) {
+        if (typeButtons[i].checked === true) {
             selectedType = typeButtons[i].value;
         }
     }
 
-    // filter the cards based selected type
-    const typeToInclude = selectedType || "Colorless";
+    // console.log(selectedType);
+    // Filter cards based on value selected
     const filteredCards = allPokeCards.filter(function (card) {
-        if (card.types.includes(typeToInclude)) {
-            return true;
-        }
-        return false;
+        return card.types.includes(selectedType);
     });
 
-    // add images for filtered cards
+    // Looping through all cards and pulling each small img link, creating an img element and assigning the link to each tag
     const newImages = [];
     for (let i = 0; i < filteredCards.length; i++) {
-        const img = document.createElement("img");
-        img.setAttribute("src", filteredCards[i].images.small);
-        newImages.push(img);
+        const imageLink = filteredCards[i].images.small;
+        const image = document.createElement("img");
+        image.setAttribute("src", imageLink);
+        newImages.push(image);
     }
+    // cards-container (index.html) is updated with new images
     cardsContainer.replaceChildren(...newImages);
 }
 
