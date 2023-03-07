1aab5c58aa792e7725bc151fa6666f85809861ad
The-Codinator-Berlin <carl.manley.berlin@gmail.com>
2023-03-07 15:48:42 +0100

    Show more/less button function: info page

--- main.js
+++ main.js
@@ -37,6 +37,23 @@ function updateCardList() {
     cardsContainer.replaceChildren(...newImages);
 }
 
+//////////////////Info page/////////////Below///////////////////////
+// Show more/ less text button function 
 
+function myFunction() {
+  const dots = document.getElementById("dots");
+  const moreText = document.getElementById("more");
+  const btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+}
 
 
